ffdd8b227e9f397fa66277c1f7439c396bceb707
Nataliia Dubrovna <natala.dubrovna@gmail.com>
2021-06-03 17:29:58 +0300

    add some comments and explanations

--- 2hw_loops_NataliiaDubrovna/script.js
+++ 2hw_loops_NataliiaDubrovna/script.js
@@ -1,46 +1,41 @@
-let N;
-let M;
+
+let N, M;
 
 function inputN(){
     do{
-        N = +prompt("Enter first integer number","Enter here integer number");
-    } while((Object.is(NaN, N) === true) || (N !== parseInt(N)))
+        N = +prompt("Enter first integer number","Enter here integer number"); // + converts string into a number  
+    } while((Object.is(NaN, N) === true) || (N !== parseInt(N))) //  || return first true or last false
 }
 function inputM(){
     do{
-        M = +prompt("Enter last number integer","Enter here integer number");
-    } while((Object.is(NaN, M) === true) || ( M !== parseInt(M)))
+        M = +prompt("Enter second integer number","Enter here integer number");
+    } while((Object.is(NaN, M) === true) || ( M !== parseInt(M))) // Object.is(NaN, NaN) ==> true ; NaN === NaN ==> false
 }
 
 inputN();
 inputM();
 
-
-let addEven = confirm('Should we add Even numbers?');
-
-
+let addEven = confirm('Should the Even numbers be added?');
 
 function sumNubersInDiapasone(n,m){
     let sum = 0; 
     let maxNumber = Math.max(n,m);
     let minNumber = Math.min(n,m); 
 
-    if(addEven){ 
+    if(addEven){  
         for(let i = minNumber; i <= maxNumber ; i++){
         sum += i;
-        }
-        return sum;
+        }return sum;
     }
     else{
         for(let i = minNumber; i <= maxNumber ; i++){
-            if(!(i % 2 == 0)){
+            if(!(i % 2 == 0)){ // skip even numbers or might be also (i % 2 == 1)
                 sum += i;
-            }
-        }
-        return sum;
+            } 
+        }return sum;
     }
 }
-
 alert(sumNubersInDiapasone(N,M));
 console.log(sumNubersInDiapasone(N,M));
+
 
